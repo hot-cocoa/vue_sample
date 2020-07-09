e37e1bf42713761961815380cca038315a4ebe0e
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-07-09 21:44:57 +0900

    add function of deleting task

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -1,6 +1,10 @@
 const app = new Vue({
   el: '#app',
   data: {
+    STATUS: {
+      todo: "ToDo",
+      doing: "Doing"
+    },
     newTask: null,
     taskCount: 0,
     todos: []
@@ -10,17 +14,30 @@ const app = new Vue({
       this.taskCount++
       this.todos.push({
         name: this.newTask,
-        checked: false
+        isDone: false,
+        status: this.STATUS.todo
       })
       this.newTask = null
     },
-    checkTask: function(index) {
-      this.taskCount--
-      this.todos[index].checked = true
+    nextTask: function(index) {
+      if (this.todos[index].status === this.STATUS.todo)
+        this.todos[index].status = this.STATUS.doing
+      else
+        this.todos[index].status = this.STATUS.todo
+    },
+    isToDo: function(status) {
+      return status === this.STATUS.todo
+    },
+    isDoing: function(status) {
+      return status === this.STATUS.doing
     },
     clearTask: function() {
       this.taskCount = 0
       this.todos = []
+    },
+    deleteTask: function(index) {
+      this.taskCount--
+      this.todos.splice(index, 1)
     }
   },
   computed: {
