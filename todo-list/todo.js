4f4877fab4be34cab72913913d0aaa64ac4f776f
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-07-09 22:48:50 +0900

    add status(todo, doing, done)

--- todo-list/todo.js
+++ todo-list/todo.js
@@ -2,30 +2,20 @@ const app = new Vue({
   el: '#app',
   data: {
     newTask: null,
-    taskCount: 0,
-    todos: []
+    todos: [],
+    doings: [],
+    dones: []
   },
   methods: {
     addTask: function() {
-      this.taskCount++
       this.todos.push({
         name: this.newTask,
         isDone: false
       })
       this.newTask = null
     },
-    clearTask: function() {
-      this.taskCount = 0
-      this.todos = []
-    },
-    deleteTask: function(index) {
-      this.taskCount--
-      this.todos.splice(index, 1)
-    }
-  },
-  computed: {
-    isEmptyTasks: function() {
-      return this.taskCount === 0
+    deleteTask: function(arr, index) {
+      arr.splice(index, 1)
     }
   }
 })
