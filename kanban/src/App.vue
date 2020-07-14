ceb462e43af7d8be846938eea82f2dec8d77f6f5
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-07-14 22:48:46 +0900

    update UI

--- kanban/src/App.vue
+++ kanban/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <div id="app">
-    <div v-for="elem in elems" :key="elem.title">
-      <h2>{{ elem.title }}</h2>
-      <div class="items-area">
-        <draggable :list="elem.items" :options="{group: 'TASKS', animation: 150}">
-          <div v-bind:key="item" v-for="item in elem.items">
-            <div>{{ item.name }}</div>
+    <div class="tasks-containers">
+      <div class="tasks-container" v-for="elem in elems" :key="elem.title">
+        <h2>{{ elem.title }}</h2>
+        <draggable class="tasks-area" :options="{group: 'TASKS', animation: 150}">
+          <div class="card" v-bind:key="item" v-for="item in elem.items">
+            {{ item.name }}
           </div>
         </draggable>
       </div>
     </div>
+
     <h2>タスク追加</h2>
     <input type="text" v-model="newTask" placeholder="タスクを入力して下さい">
     <button v-on:click="addTask">追加</button>
@@ -59,11 +60,33 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  text-align: center;
   margin-top: 60px;
 }
-.items-area {
-  min-height: 15px;
+.tasks-containers {
+  display: flex;
+  justify-content: center;
+}
+.tasks-container {
+  min-width: 500px;
+  max-width: 500px;
+  min-height: 400px;
+  width: 90%;
+  border: solid #888888 1.5px;
+  justify-content: center;
+  align-items: center;
+}
+.tasks-area {
+  justify-content: center;
+  align-items: center;
+}
+.card {
+  font-size: 12px;
+  padding: 10px;
+  cursor: pointer;
+  border: solid #cccccc 1.5px;
+  border-radius: 5px;
+  width: 80%;
 }
 </style>
