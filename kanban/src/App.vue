d1f606d2f8bb90364433b18a1ba582391f1da5df
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-07-17 23:51:34 +0900

    update UI

--- kanban/src/App.vue
+++ kanban/src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div id="app">
     <div class="tasks-containers">
-      <div class="tasks-container" v-for="elem in elems" :key="elem.title">
-        <h2>{{ elem.title }}</h2>
-        <draggable class="tasks-area" :options="{group: 'TASKS', animation: 150}">
+      <div class="tasks-container" v-for="(elem, index) in elems" :key="elem.title">
+        <div class="container-header">
+          <h2 class="header-title">{{ elem.title }}</h2>
+          <button class="header-button" @click="show(index)">+</button>
+          <modal v-if="elem.show" name="add-task" :draggable="true" :resizable="true">
+            <div class="modal-header">
+              <h2 class="header-title">New Task</h2>
+              <button class="header-button" @click="hide(index)">×</button>
+            </div>
+            <div class="modal-body">
+              <input type="text" v-model="newTask" placeholder="Please input task">
+              <button v-on:click="addTask(elem.items)">追加</button>
+            </div>
+          </modal>
+        </div>
+        <draggable class="container-body" :options="{group: 'TASKS', animation: 150}">
           <div v-bind:key="item" v-for="(item, index) in elem.items">
             <div class="item">
               <div class="card">{{ item.name }}</div>
@@ -13,10 +26,6 @@
         </draggable>
       </div>
     </div>
-
-    <h2>タスク追加</h2>
-    <input type="text" v-model="newTask" placeholder="タスクを入力して下さい">
-    <button v-on:click="addTask">追加</button>
   </div>
 </template>
 
@@ -34,28 +43,39 @@ export default {
       elems: [
         {
           title: "ToDo",
-          items: []
+          items: [],
+          show: false
         },
         {
           title: "Doing",
-          items: []
+          items: [],
+          show: false
         },
         {
           title: "Done",
-          items: []
+          items: [],
+          show: false
         }
       ]
     }
   },
   methods: {
-    addTask: function() {
-      this.elems[0].items.push({
+    addTask: function(items) {
+      items.push({
         name: this.newTask,
       })
       this.newTask = null
     },
     deleteTask: function(items, index) {
       items.splice(index, 1)
+    },
+    show: function(index) {
+      this.$modal.show('add-task')
+      this.elems[index].show = true
+    },
+    hide: function(index) {
+      this.$modal.hide('add-task')
+      this.elems[index].show = false
     }
   }
 }
@@ -77,25 +97,51 @@ export default {
 .tasks-container {
   min-width: 500px;
   max-width: 500px;
-  min-height: 400px;
-  border: solid #888888 1.5px;
+  min-height: 700px;
+  border: solid #888888 5px;
   justify-content: center;
   align-items: center;
 }
-.tasks-area {
+.header-title {
+  color: #ffffff;
+}
+.header-button {
+  position: absolute;
+  right: 0;
+}
+.container-header {
+  display: flex;
+  justify-content: center;
+  position: relative;
+  background: #222222;
+}
+.modal-body {
+  margin: 55px;
+}
+.modal-header {
+  border-bottom: 1px solid #ddd;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  background: #222222;
+}
+.container-body {
   justify-content: center;
   align-items: center;
 }
 .item {
+  display: flex;
+  min-width: 400px;
   margin-left: auto;
   margin-right: auto;
-  width: 80%;
 }
 .card {
-  font-size: 12px;
+  font-size: 20px;
   padding: 10px;
   cursor: pointer;
   border: solid #cccccc 1.5px;
   border-radius: 5px;
+  width: 95%;
+  min-height: 20px;
 }
 </style>
