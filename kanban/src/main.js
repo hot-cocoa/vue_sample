546b847d0ced4796f4064f81cbefe11d13b23114
hot-cocoa <hotcocoa.cappuccino.caffemocha@gmail.com>
2020-07-19 18:30:47 +0900

    add local storage

--- kanban/src/main.js
+++ kanban/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VModal from 'vue-js-modal'
+import VueLocalStorage from 'vue-localstorage'
 
 Vue.config.productionTip = false
 Vue.use(VModal)
+Vue.use(VueLocalStorage)
 
 new Vue({
   render: h => h(App),
